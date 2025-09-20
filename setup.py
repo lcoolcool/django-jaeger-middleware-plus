032df054ae5b75dbeb722498a6e56f2490fd8b86
zhaishuaishuai <shuaishuai_zhai@163.com>
2025-09-20 13:54:15 +0800

    v0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="django-jaeger-middleware-plus",
-    version="0.1.4",
+    version="0.1.5",
     author="zhaishuaishuai",
     author_email="zhaishuaishuai001@gmail.com",
     description="A Django middleware for distributed tracing with Jaeger",
